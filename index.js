955d44d2457b5eb4f5f3021a7cd31c175535f09d
emhgit <eharper0815@gmail.com>
2025-05-30 16:56:31 -0500

    add commands to bot

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const { Telegraf } = require('telegraf')
 require("dotenv").config();
 
-const bot = new Telegraf(process.env.BOT_API_TOKEN)
-bot.command('oldschool', (ctx) => ctx.reply('Hello'))
-bot.command('hipster', Telegraf.reply('λ'))
+const commands = [
+    {command: "quit", description: "Quit chat interaction with the bot"},
+    {command: "post", description: "Make a post with the bot"},
+    {command: "help", description: "View help options"},
+];
+const bot = new Telegraf(process.env.BOT_API_TOKEN);
+bot.telegram.setMyCommands(commands);
 
-bot.launch()
+//quit function
+bot.command('quit', async (ctx) => {
+  // Explicit usage
+  await ctx.telegram.leaveChat(ctx.message.chat.id);
+
+  // Using context shortcut
+  await ctx.leaveChat();
+  console.log(`${process.env.BOT_FIRST_NAME} has quit`);
+});
+
+bot.command('post', (ctx) => {
+    console.log(ctx);
+    ctx.reply('What will be the name of your post?');
+});
+
+bot.help((ctx) => ctx.reply('Help message'));
+bot.hears('hello bot', (ctx) => ctx.reply('Hello world!'));
+
+bot.launch();
 
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
