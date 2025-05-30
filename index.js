83b98aa76e47d1ca3edfa4aa86358b8d49fae2a6
emhgit <eharper0815@gmail.com>
2025-05-30 16:16:21 -0500

    install dotenv

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const { Telegraf } = require("telegraf");
+const { Telegraf } = require('telegraf')
+require("dotenv").config();
 
-const bot = new Telegraf()
+const bot = new Telegraf(process.env.BOT_API_TOKEN)
+bot.command('oldschool', (ctx) => ctx.reply('Hello'))
+bot.command('hipster', Telegraf.reply('λ'))
+
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
